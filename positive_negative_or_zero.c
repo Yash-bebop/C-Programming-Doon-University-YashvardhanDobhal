57c44c33e0b30b8df1ac0860cd27b2fba17657cf
Yash-bebop <146559706+Yash-bebop@users.noreply.github.com>
2025-09-12 23:39:50 +0530

    Add files via upload

---
+++ positive_negative_or_zero.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int num;
+    printf("Enter The Number: ");
+    scanf("%d" , &num);
+if (num>0)
+{
+    printf("The number is positive\n");
+}
+else if (num==0)
+    {
+        printf("You've input zero!\n");
+    }
+else
+{
+    printf("The number is negative\n");
+}
+return 0;
+}
