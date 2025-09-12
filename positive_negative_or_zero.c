bb750c7473c4caf4f41d7a4349f60b7d4cb4be77
Yash-bebop <146559706+Yash-bebop@users.noreply.github.com>
2025-09-13 00:27:50 +0530

    Update positive_negative_or_zero.c

--- positive_negative_or_zero.c
+++ positive_negative_or_zero.c
@@ -4,14 +4,15 @@ int main()
 int num;
     printf("Enter The Number: ");
     scanf("%d" , &num);
+
 if (num>0)
 {
     printf("The number is positive\n");
 }
 else if (num==0)
-    {
-        printf("You've input zero!\n");
-    }
+{
+    printf("You've input zero!\n");
+}
 else
 {
     printf("The number is negative\n");
