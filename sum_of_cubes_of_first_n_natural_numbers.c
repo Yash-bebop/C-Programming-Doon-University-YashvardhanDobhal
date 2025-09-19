8cdd1cfa779c29272556451f8fa6eeeba60239c2
Yash-bebop <146559706+Yash-bebop@users.noreply.github.com>
2025-09-19 10:07:27 +0530

    Create sum_of_cubes_of_first_n_natural_numbers.c

---
+++ sum_of_cubes_of_first_n_natural_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,sum=0;
+    printf("Enter the number upto which you want a sum of cubes 1-n: ");
+    scanf("%d" , &a);
+    for(int i=1 ; i<=a ; i++)
+    {
+        sum=sum+(i*i*i);
+    }
+        printf("The sum is: %d\n" , sum);
+    return 0;
+}
